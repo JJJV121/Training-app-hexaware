fd4ff9ac1b336f91833dfa1090a2895e6d3f824a
iamkavithasr <kavithasri262801@gmail.com>
2026-07-31 09:43:20 +0530

    slight change in session.py removed unused line

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -13,9 +13,7 @@ from app.core.config import settings
 
 ssl_context = ssl.create_default_context()
 
-print("=" * 80)
-print("DATABASE_URL:", settings.DATABASE_URL)
-print("=" * 80)
+
 
 db_url = settings.DATABASE_URL
 
